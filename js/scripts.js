862df4974afdf9daad22b54c7777f0d488783c20
Jim Patt <wrench1138@gmail.com>
2015-05-29 10:44:57 -0700

    add a test for cutting out a substring

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,25 @@ var findAndReplace = function(originalString, foundWord, replacementWord) {
   var outputString = originalString.replace(foundWord, replacementWord);
   return outputString;
 }
+
+// NOTE: So... the above code works just fine. It turns out that a find and
+//       replace method already exists. I however feel like the spirit of
+//       the exercise is to figure out how to write my own method to do the
+//       same thing. Seeing as I have 5+ more hours to kill I think I'll do
+//       just that :)
+
+
+
+$(document).ready(function() {
+  $("form#phrases").submit(function(event) {
+    var originalString = ($("input#original_string").val());
+    var foundWord = ($("input#found_word").val());
+    var replacementWord = ($("input#replacement_word").val());
+    var outputString = findAndReplace(originalString, foundWord, replacementWord);
+
+    $(".output").text(outputString);
+
+    $("#result").show();
+    event.preventDefault();
+  })
+})
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,15 @@
 describe('findAndReplace', function() {
   it('finds a particular string of words in a larger string and replaces it with an alternate substring', function() {
-    var testString = "my mother and I were sitting on the porch."
+    var testString = "my mother and I were sitting on the porch.";
     expect(findAndReplace(testString, "mother", "father")).to.eql("my father and I were sitting on the porch.");
+  });
+});
+
+describe('findAndReplaceHardMode', function() {
+  describe('cut', function() {
+    it('will search through a string and cut out a substring', function() {
+      var testString = "my mother and I were sitting on the porch.";
+      expect(cut(testString, "mother")).to.equal("my  and I were sitting on the porch.");
+    });
   });
 });
