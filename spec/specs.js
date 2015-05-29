e5a7d93f525565d414eae56f88e40d1cf4783246
Jim Patt <wrench1138@gmail.com>
2015-05-29 09:27:31 -0700

    edit spec so that it actually runs

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
-describe('findAndReplace', function {
-  it('finds a particular string of words in a larger string and replaces it with an alternate substring', {
+describe('findAndReplace', function() {
+  it('finds a particular string of words in a larger string and replaces it with an alternate substring', function() {
     var testString = "my mother and I were sitting on the porch."
     expect(findAndReplace("mother", "father")).to.equal("my father and I wre sitting on the porch");
   });
