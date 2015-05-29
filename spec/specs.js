55e50abb6f7ba9dcf381842fea3dfd6901bd617f
Jim Patt <wrench1138@gmail.com>
2015-05-29 11:21:06 -0700

    add a test to find and replace the hard way

--- spec/specs.js
+++ spec/specs.js
@@ -16,5 +16,12 @@ describe('insert', function() {
   it('will insert a string into another string at a given position', function() {
     var testString = "my  and I were sitting on the porch.";
     expect(insert(testString, "mother", 3)).to.equal("my mother and I were sitting on the porch.");
-  })
-})
+  });
+});
+
+describe('findAndReplaceHardMode', function() {
+  it('will find and replace a section of a string the hard way', function() {
+    var testString = "my mother and I were sitting on the porch.";
+    expect(findAndReplaceHardMode(testString, "mother", "father")).to.eql("my father and I were sitting on the porch.");
+  });
+});
